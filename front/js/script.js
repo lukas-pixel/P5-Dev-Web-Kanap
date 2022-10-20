e346082b4de4fb9b6b48334b91d2012b4085a7c0
luk3s <luk3s.perzival@gmail.com>
2022-10-20 16:52:30 +0200

    create requete API + recovery of the requete

--- front/js/script.js
+++ front/js/script.js
@@ -1,2 +1,18 @@
 "use strict";
 
+getProduct();
+
+function getProduct (){
+    fetch("http://localhost:3000/api/products")
+        .then(res => res.json())
+        .then((data) => arrayProduct(data));
+}
+
+function arrayProduct(canapes) {
+    for (let i = 0; i < canapes.length; i++) {
+        const canapeInfo = canapes[i];
+    }
+}
+
+// création de "a"
+
