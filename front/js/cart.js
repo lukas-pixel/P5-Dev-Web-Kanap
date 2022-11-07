40016e750d466bd5b96986d5ce13e7ccd0af09a7
mcherdo <marine.cherdo13@gmail.com>
2022-11-07 15:37:24 +0100

    refactoring adn change name function

--- front/js/cart.js
+++ front/js/cart.js
@@ -23,36 +23,90 @@ async function sectionCartItem(){
 sectionCartItem();
 
 function creationDom(item, product) {
-//élements Delete---------------------------------------------------------
-     //creation p Delete
-    let pDeleteItem = document.createElement("p");
-    pDeleteItem.classList.add("deleteItem");
-    pDeleteItem.innerText = "Supprimer";
-    // creation div delete + ajout de l'enfant p delete
-    let divContentSettingsDelete = document.createElement("div");
-    divContentSettingsDelete.classList.add("cart__item__content__settings__delete");
-    divContentSettingsDelete.appendChild(pDeleteItem);
+//creation p Delete
+    let itemDelete = document.createElement("p");
+    itemDelete.classList.add("deleteItem");
+    itemDelete.innerText = "Supprimer";
+// creation div delete + ajout de l'enfant p delete
+    let cartSettings = document.createElement("div");
+    cartSettings.classList.add("cart__item__content__settings__delete");
+    cartSettings.appendChild(itemDelete);
 
-//element Changement quantité---------------------------------------------------------
-    //creation input
-    let inputQuantity = document.createElement("input");
-    inputQuantity.type = "number";
-    inputQuantity.classList.add("itemQuantity");
-    inputQuantity.name = "itemQuantity";
-    inputQuantity.min = "1";
-    inputQuantity.max = "100";
-    inputQuantity.value = product.product_quantity;
-    // creation p quantity
-    let pQuantity = document.createElement("p");
-    pQuantity.innerText = "Qté : ";
-    //creation div quantity + ajout enfant input + p quantity
-    let divContentSettingsQuantity = document.createElement("div");
-    divContentSettingsQuantity.classList.add("cart__item__content__settings__quantity");
-    divContentSettingsQuantity.appendChild(pQuantity);
-    divContentSettingsQuantity.appendChild(inputQuantity);
+//action supprimer un article
+    addDeleteItem(itemDelete, cartItem, item, product);
 
-    //Au changement de la quantite----------------------------------------------------------------
-    inputQuantity.addEventListener("change", (e) => {
+
+//creation input
+    let quantityNumber = document.createElement("input");
+    quantityNumber.type = "number";
+    quantityNumber.classList.add("itemQuantity");
+    quantityNumber.name = "itemQuantity";
+    quantityNumber.min = "1";
+    quantityNumber.max = "100";
+    quantityNumber.value = product.product_quantity;
+// creation p quantity
+    let quantity = document.createElement("p");
+    quantity.innerText = "Qté : ";
+//creation div quantity + ajout enfant input + p quantity
+    let cartQuantity = document.createElement("div");
+    cartQuantity.classList.add("cart__item__content__settings__quantity");
+    cartQuantity.appendChild(quantity);
+    cartQuantity.appendChild(quantityNumber);
+
+//Action changement quantité
+    addChangeQuantity(quantityNumber, item, product);
+
+//creation div Content Settings + ajout enfant div quantity + div Delete
+    let cartSettingsQuantity = document.createElement("div");
+    cartSettingsQuantity.classList.add("cart__item__content__settings");
+    cartSettingsQuantity.appendChild(cartSettings);
+    cartSettingsQuantity.appendChild(cartQuantity);
+
+//creation p price de div Content description
+    let cartPrice = document.createElement("p");
+    cartPrice.innerText = item.price + " €";
+//creation p color de div content description
+    let cartColor = document.createElement("p");
+    cartColor.innerText = product.product_color;
+//creation h2
+    let cartName = document.createElement("h2");
+    cartName.innerText = item.name;
+//creation div description + ajouts enfants
+    let cartContentDescription = document.createElement("div");
+    cartContentDescription.classList.add("cart__item__content__description");
+    cartContentDescription.appendChild(cartName);
+    cartContentDescription.appendChild(cartColor);
+    cartContentDescription.appendChild(cartPrice);
+    
+//creation cartItemContent + ajouts enfants
+    let cartItemContent = document.createElement("div");
+    cartItemContent.classList.add("cart__item__content");
+    cartItemContent.appendChild(cartContentDescription);
+    cartItemContent.appendChild(cartSettingsQuantity);
+
+//creation img
+    let imgItem = document.createElement("img");
+    imgItem.src = item.imageUrl;
+    imgItem.alt = item.altTxt;
+//Creation cartImg + ajouts enfants
+    let cartImg = document.createElement("div");
+    cartImg.classList.add("cart__item__img");
+    cartImg.appendChild(imgItem);
+
+//creation article + ajouts enfants
+    let cartItem = document.createElement("article");
+    cartItem.classList.add("cart__item");
+    cartItem.appendChild(cartImg);
+    cartItem.appendChild(cartItemContent);
+
+    //rattachement a la section existante
+    let sectionItem = document.getElementById("cart__items");
+    sectionItem.appendChild(cartItem);
+
+}
+
+function addChangeQuantity(quantityNumber, item, product) {
+    quantityNumber.addEventListener("change", (e) => {
         let panierLocalStorage = JSON.parse(localStorage.getItem("ProductCart"));
 
         panierLocalStorage = panierLocalStorage.map(item => {
@@ -64,63 +118,17 @@ function creationDom(item, product) {
 
         localStorage.setItem("ProductCart", JSON.stringify(panierLocalStorage))
     })
+}
 
-    //creation div Content Settings + ajout enfant div quantity + div Delete
-    let divContentSettings = document.createElement("div");
-    divContentSettings.classList.add("cart__item__content__settings");
-    divContentSettings.appendChild(divContentSettingsDelete);
-    divContentSettings.appendChild(divContentSettingsQuantity);
-
-    //creation p price de div Content description
-    let pDescriptionPrice = document.createElement("p");
-    pDescriptionPrice.innerText = item.price + " €";
-    //creation p color de div content description
-    let pDescriptionColor = document.createElement("p");
-    pDescriptionColor.innerText = product.product_color;
-    //creation h2
-    let productTitle = document.createElement("h2");
-    productTitle.innerText = item.name;
-    //creation div description + ajouts enfants
-    let divContentDescription = document.createElement("div");
-    divContentDescription.classList.add("cart__item__content__description");
-    divContentDescription.appendChild(productTitle);
-    divContentDescription.appendChild(pDescriptionColor);
-    divContentDescription.appendChild(pDescriptionPrice);
-
-    //creation divItemContent + ajouts enfants
-    let divItemContent = document.createElement("div");
-    divItemContent.classList.add("cart__item__content");
-    divItemContent.appendChild(divContentDescription);
-    divItemContent.appendChild(divContentSettings);
-
-    //creation img
-    let productImg = document.createElement("img");
-    productImg.src = item.imageUrl;
-    productImg.alt = item.altTxt;
-    //Creation divItemImg + ajouts enfants
-    let divItemImg = document.createElement("div");
-    divItemImg.classList.add("cart__item__img");
-    divItemImg.appendChild(productImg);
-
-    //creation article + ajouts enfants
-    let articleCart = document.createElement("article");
-    articleCart.classList.add("cart__item");
-    articleCart.appendChild(divItemImg);
-    articleCart.appendChild(divItemContent);
-
-    //rattachement a la section existante
-    let sectionItem = document.getElementById("cart__items");
-    sectionItem.appendChild(articleCart);
-
+function addDeleteItem(itemDelete, cartItem, item, product) {
     //Suppression d'un element
-    pDeleteItem.addEventListener("click", () => {
+    itemDelete.addEventListener("click", () => {
 
         let panierLocalStorage = JSON.parse(localStorage.getItem("ProductCart"));
 
         panierLocalStorage = panierLocalStorage.filter(item => item.product_id !== product.product_id || item.product_color !== product.product_color);
         localStorage.setItem("ProductCart", JSON.stringify(panierLocalStorage));
         alert("Votre article a bien été supprimé");
-        articleCart.remove();
+        cartItem.remove();
     })
-
 }
